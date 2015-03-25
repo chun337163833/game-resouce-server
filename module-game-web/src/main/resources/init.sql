7a054d090be620c45a093074d47d6b7d144ed8c6
herisn23 <herisn23@gmail.com>
2015-03-25 17:53:15 +0100

    application works, except jpqlfilter, need create entitymanageraccessors

--- module-game-web/src/main/resources/init.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO role(id, code) VALUES (nextval('role_id_seq'), 'ROLE_ADMIN');
-INSERT INTO role(id, code) VALUES (nextval('role_id_seq'), 'ROLE_CUSTOMER');
-INSERT INTO role(id, code) VALUES (nextval('role_id_seq'), 'ROLE_DRIVER');
-INSERT INTO role(id, code) VALUES (nextval('role_id_seq'), 'ROLE_DISPATCHER');
-
-INSERT INTO tx_user(dtype, id, email, first_name, last_name, password, user_name, version) VALUES('OtherUser', nextval('tx_user_id_seq'), 'admin@localhost', 'admin', 'admin', 'af403d2b497aa62dadfddf02282b19fd40496898d4643e273ba52a9d467653b2', 'admin', 0);
-INSERT INTO tx_user_roles(tx_user, roles) VALUES ((select id from tx_user where user_name='admin'), (select id from role where code='ROLE_ADMIN'));
-
-commit
